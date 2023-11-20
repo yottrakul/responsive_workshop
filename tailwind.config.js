f415b812e50cc46e2ff1723ea11e6ff759e1eb4d
Yottrakul Suwannasri <yottrakul_s.vo@tripetch-it.co.th>
2023-11-20 14:36:24 +0700

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#5C3CAF',
+      'secondary': '#F5F4F9',
+      'gray': '#E7E7E7',
+      'dark-gray': '#C2C2C2',
+      'white': '#FFF'
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
